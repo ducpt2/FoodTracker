f03745ef5a8f24f761843548d243c47c48fda66c
DucPT2 <ducft.nb@gmail.com>
2016-09-04 22:11:22 +0700

    Build Basic UI.
    Building a basic UI for FoodTracker app.

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by DucPT2 on 9/4/16.
+//  Copyright © 2016 DucPT2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
